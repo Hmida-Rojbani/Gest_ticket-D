18debc52065d0e8f738e21527841d31df426b546
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-08 14:11:55 +0100

    first Table

---
+++ src/main/java/de/tekup/rst/GestTicketDApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.rst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestTicketDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestTicketDApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/tekup/rst/entities/TableType.java
@@ -0,0 +1,6 @@
+package de.tekup.rst.entities;
+
+public enum TableType {
+
+	OUTSIDE,INSIDE_SMOKER,INSIDE_NO_SMOKER
+}
